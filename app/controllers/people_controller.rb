5ffd5a6d49419ad65d6623c7181e437d50197d1a
Yuichi-Murata01 <yuichi121201@gmail.com>
2021-07-27 08:33:44 +0900

    スライダーでつまずき中です！

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -8,4 +8,20 @@ class PeopleController < ApplicationController
     @msg = 'indexed data.'
     @data = Person.find(params[:id])
   end
+
+  def add
+    @msg = "add new data."
+  end
+
+  def create
+    if request.post? then
+      obj = Person.create(
+        name: params['name'],
+        age: params['age'],
+        mail: params['mail']
+      )
+    end
+    redirect_to '/people'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users' ,to: 'users#index'
   get 'people',to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
   get 'people/:id', to: 'people#show'
   get '/' => 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
